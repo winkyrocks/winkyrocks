776fb6c3184ce290e345b62e4996d7af52d9f0f7
Ke Wang <kewang1825@gmail.com>
2016-08-29 20:58:27 -0700

    calculate largest rectangle

---
+++ test10/main.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <iostream>
+#include <stack>
+#include <algorithm>
+
+bool IsNewPillarOrReachEnd(const std::vector<int> &heights, int curr_idx, int last_pillar_idx)
+{
+    return curr_idx < (int)heights.size() ? heights[curr_idx] < heights[last_pillar_idx] : true;
+}
+
+int CalculateLargestRectangle(const std::vector<int> &heights)
+{
+    std::stack<int> pillar_indices;
+    int max_rectangle_area = 0;
+    for (int i = 0; i <= (int)heights.size(); ++i)
+    {
+//        if (!pillar_indices.empty() && i < heights.size() && heights[i] == heights[pillar_indices.top()])
+//        {
+//            std::cout << i << "REPLACE" << pillar_indices.top() << std::endl;
+//            pillar_indices.pop();
+//            pillar_indices.emplace(i);
+//        }
+
+        while (!pillar_indices.empty() && IsNewPillarOrReachEnd(heights, i, pillar_indices.top()))
+        {
+            int height = heights[pillar_indices.top()];
+            std::cout << i << ": POP=" << pillar_indices.top() << std::endl;
+            pillar_indices.pop();
+            int width = pillar_indices.empty() ? i : i - pillar_indices.top() - 1;
+            max_rectangle_area = std::max(max_rectangle_area, height * width);
+            std::cout << i << ": MAX=" << max_rectangle_area << ";" << height << "*" << width << std::endl;
+        }
+
+        pillar_indices.emplace(i);
+        std::cout << i << ":PUSH=" << pillar_indices.top() << std::endl;
+    }
+
+    return max_rectangle_area;
+}
+
+
+int main()
+{
+    std::vector<int> heights = { 1, 4, 2, 5, 6, 3, 2, 6, 6, 5, 2, 1, 3 };
+    int r = CalculateLargestRectangle(heights);
+    std::cout << r << std::endl;
+}
